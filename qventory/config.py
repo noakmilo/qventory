b0922d5f083212083b36def642dcbe83c0ab1eeb
Camilo Noa <camilo@slyk.io>
2025-09-13 14:33:17 -0700

    update

--- qventory/config.py
+++ qventory/config.py
@@ -1,10 +1,7 @@
-# qventory/config.py
 import os, pathlib
-
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "change-me")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
     _db_path = os.environ.get("QVENTORY_DB_PATH")
     if _db_path:
         p = pathlib.Path(_db_path)
