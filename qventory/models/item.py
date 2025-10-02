f85aca1a9c64ea3d3d5b1549c458458732b4dc27
Camilo Noa <camilo@slyk.io>
2025-10-01 18:47:21 -0700

    Update item.py

--- qventory/models/item.py
+++ qventory/models/item.py
@@ -10,7 +10,6 @@ class Item(db.Model):
     sku = db.Column(db.String, unique=True, nullable=False)
     listing_link = db.Column(db.String, nullable=True)
 
-    # URLs de venta
     web_url = db.Column(db.String, nullable=True)
     ebay_url = db.Column(db.String, nullable=True)
     amazon_url = db.Column(db.String, nullable=True)
@@ -19,19 +18,10 @@ class Item(db.Model):
     poshmark_url = db.Column(db.String, nullable=True)
     depop_url = db.Column(db.String, nullable=True)
 
-    # Ubicación
     A = db.Column(db.String, nullable=True)
     B = db.Column(db.String, nullable=True)
     S = db.Column(db.String, nullable=True)
     C = db.Column(db.String, nullable=True)
 
     location_code = db.Column(db.String, index=True)
-
-    # Nuevos campos
-    item_thumb = db.Column(db.String, nullable=True)       # URL de Cloudinary
-    supplier = db.Column(db.String, nullable=True)         # Nombre de la tienda
-    item_cost = db.Column(db.Float, nullable=True)         # Costo en $
-    item_price = db.Column(db.Float, nullable=True)        # Precio en $
-    listing_date = db.Column(db.Date, nullable=True)       # Fecha de publicación
-
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
