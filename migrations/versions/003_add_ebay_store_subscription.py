c834192835e71a9f51ea2c2bf0d5084b0c2ff24c
Camilo Noa <camilo@slyk.io>
2025-10-09 23:03:30 -0700

    Update 003_add_ebay_store_subscription.py

--- migrations/versions/003_add_ebay_store_subscription.py
+++ migrations/versions/003_add_ebay_store_subscription.py
@@ -1,7 +1,7 @@
 """add ebay store subscription
 
 Revision ID: 003_add_ebay_store_subscription
-Revises: 001_initial_schema
+Revises: 002_add_last_login
 Create Date: 2025-10-09 22:49:00.000000
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '003_add_ebay_store_subscription'
-down_revision = '001_initial_schema'
+down_revision = '002_add_last_login'
 branch_labels = None
 depends_on = None
 
