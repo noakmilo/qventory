b08f7e3adbc74d57745a97ed14721d9f16f015c6
Camilo Noa <camilo@slyk.io>
2026-01-17 22:58:36 -0800

    Update 035_add_ebay_top_rated_to_credentials.py

--- migrations/versions/035_add_ebay_top_rated_to_credentials.py
+++ migrations/versions/035_add_ebay_top_rated_to_credentials.py
@@ -1,6 +1,6 @@
 """Add ebay_top_rated to marketplace_credentials.
 
-Revision ID: 035_add_ebay_top_rated_to_credentials
+Revision ID: 035_add_ebay_top_rated
 Revises: 034_add_sale_tax_collected
 Create Date: 2025-01-11 00:00:00.000000
 """
@@ -10,14 +10,20 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '035_add_ebay_top_rated_to_credentials'
+revision = '035_add_ebay_top_rated'
 down_revision = '034_add_sale_tax_collected'
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    op.add_column('marketplace_credentials', sa.Column('ebay_top_rated', sa.Boolean(), nullable=True, server_default=sa.text('false')))
+    bind = op.get_bind()
+    inspector = sa.inspect(bind)
+    existing_columns = {column["name"] for column in inspector.get_columns('marketplace_credentials')}
+
+    if 'ebay_top_rated' not in existing_columns:
+        op.add_column('marketplace_credentials', sa.Column('ebay_top_rated', sa.Boolean(), nullable=True, server_default=sa.text('false')))
+
     op.alter_column('marketplace_credentials', 'ebay_top_rated', server_default=None)
 
 
